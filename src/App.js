a12e6757176955a752fb18779e81ac0e702dcff9
Nataliia Arsenieva <nataly.arsenieva@gmail.com>
2022-02-05 02:50:57 -0500

    Implemented simple routing

--- src/App.js
+++ src/App.js
@@ -1,25 +1,48 @@
-import logo from './logo.svg';
 import './App.css';
+import React from "react";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link, useRouteMatch
+} from "react-router-dom";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+function App()
+{
+    //let match = useRouteMatch({ path: to, extact: true });
+    
+    return (
+        <Router>
+            <div>
+            <h1>Simple Dynamic Routing!</h1>
+            <div>
+            <Link to="/">Home</Link>
+            <Link to="/comment">Comment</Link>
+            <Link to="/thankyou">Thank You</Link>
+            </div>
+            <div>
+            <Switch>
+                <Route exact path="/" component={Login} />
+                <Route exact path="/comment" component={Comment} />
+                <Route exact path="/thankyou" component={ThankYou} />
+            </Switch>
+            </div>
+        </div>
+      </Router>
+    );
+    //
+}
+
+function Login() {
+  return <h2>Home</h2>;
+}
+
+function Comment() {
+  return <h2>Comment</h2>;
+}
+
+function ThankYou() {
+  return <h2>Thank You!</h2>;
 }
 
 export default App;
